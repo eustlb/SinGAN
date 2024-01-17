36b3615acdaa323170c22410412754546c144e99
mkondracki <80763834+mkondracki@users.noreply.github.com>
2024-01-17 23:39:31 +0100

    Update datasets.py

--- generation/data/datasets.py
+++ generation/data/datasets.py
@@ -5,10 +5,11 @@ from torchvision import transforms
 from PIL import Image
 
 class Dataset(torch.utils.data.dataset.Dataset):
-    def __init__(self, root='', batch_size=1, crop_size=0):
+    def __init__(self, root='', batch_size=1, crop_size=0, custom=0):
         self.root = root
         self.batch_size = batch_size
         self.crop_size = crop_size
+        self.custom = custom
         self._init()
 
     def _init(self):
@@ -30,30 +31,62 @@ class Dataset(torch.utils.data.dataset.Dataset):
 
         # position
         w_size, h_size = size
+
+        #if random.randint(0,3) >0 : 
         x = random.randint(0, max(0, w_size - self.crop_size))
         y = random.randint(0, max(0, h_size - self.crop_size))
 
-        # flip
         flip = random.random() > 0.5
+
         return {'pos': (x, y), 'flip': flip}
 
     def _augment(self, image, aug_params, scale=1):
         x, y = aug_params['pos']
+        
         image = image[:, round(x * scale):(round(x * scale) + round(self.crop_size * scale)), round(y * scale):(round(y * scale) + round(self.crop_size * scale))]
+        #print("shape : [{}:{}, {}:{}] ,  x,y : [{},{}]  , scale : {}  , crop : {}".format(round(x * scale), (round(x * scale) + round(self.crop_size * scale)), round(y * scale), 
+        #(round(y * scale) + round(self.crop_size * scale)), x, y, scale, self.crop_size))
         if aug_params['flip']:
             image = image.flip(-1)
+        return image
+    
+    def _cust_augment(self, image, aug_params, scale=1):
+        if random.randint(0,3) >2 : 
+          #print("--0--")
+          x, y = aug_params['pos']
+        else : 
+          x, y = (0, 0)
+
+        #print("shape : [{}:{}, {}:{}] ,  x,y : [{},{}]  , scale : {}  , crop : {}".format(round(x * scale), (round(x * scale) + round(self.crop_size * scale)), round(y * scale), 
+        #(round(y * scale) + round(self.crop_size * scale)), x, y, scale, self.crop_size))
+        image = image[:, round(x * scale):(round(x * scale) + round(self.crop_size * scale)), round(y * scale):(round(y * scale) + round(self.crop_size * scale))]
+
+        if aug_params['flip']:
+            image = image.flip(-1)
+
         return image
 
     def __getitem__(self, index):
         amps = self.amps
 
+        #print("self.image.size()[-2:] : ", self.image.size()[-2:])
+
         # cropping
         if self.crop_size:
             reals, noises = {}, {}
             aug_params = self._get_augment_params(self.image.size()[-2:])
+            #print("image size : ", self.image.size()[-2:])
 
             for key in self.reals.keys():
-                scale = self.reals[key].size(-1) / float(self.image.size(-1))
+              scale = self.reals[key].size(-1) / float(self.image.size(-1))
+              #print("scaled image : ", self.reals[key].size(-1))
+              
+              if self.custom: 
+                reals.update({key: self._cust_augment(self.reals[key].clone(), aug_params, scale)})
+                noises.update({key: self._cust_augment(self.noises[key].clone(), aug_params, scale)})
+              
+              else : 
+                #print("Size : ", self.reals[key].size())
                 reals.update({key: self._augment(self.reals[key].clone(), aug_params, scale)})
                 noises.update({key: self._augment(self.noises[key].clone(), aug_params, scale)})
 
